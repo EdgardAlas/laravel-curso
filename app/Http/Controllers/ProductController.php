aa5ddf4196268c84063d9265e881851a849c4cca
Edgard Alas <edgardalasu@gmail.com>
2021-12-24 14:55:21 -0600

    Moelos y db builder

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,39 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+
 class ProductController extends Controller
 {
     public function index()
     {
+        // $product = DB::table(("products"))->get();
+        $product = Product::all();
+        dd($product);
         return view("products.index");
     }
 
     public function create()
     {
         return "This is the form to create products";
-
     }
     public function store()
     {
-
     }
     public function show($product)
     {
+        // $products = DB::table(("products"))->find($product);
+        $products = Product::findOrFail($product);
+        dd($products);
         return view("products.show");
     }
 
     public function edit($product)
     {
         return "Show the product with id {$product} to edit";
-
     }
     public function update($product)
     {
-
     }
     public function destroy($product)
     {
         return "Show the product with id {$product} to delete";
-
     }
 }
